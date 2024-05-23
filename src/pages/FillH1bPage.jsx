2a4374a85f8f4474240bd2c438e86b3ad2c5ab6e
MuskanMallZeus <165341068+MuskanMallZeus@users.noreply.github.com>
2024-05-23 13:19:06 -0500

    with the dashboard

---
+++ src/pages/FillH1bPage.jsx
@@ -0,0 +1,5 @@
+import SetH1bV3 from "../componenets/forms/SetH1bV3";
+
+export default function FillH1bPage() {
+  return <SetH1bV3 />;
+}
---
+++ src/pages/UpdateH1bPage.jsx
@@ -0,0 +1,5 @@
+import SetH1bV3 from "../componenets/forms/SetH1bV3";
+
+export default function UpdateH1bPage() {
+  return <SetH1bV3 />;
+}
